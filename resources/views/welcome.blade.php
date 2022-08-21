5c0c135461c65be1ef088a0e83066c1450b27281
Фарид Фарид <f.farenco@gmail.com>
2022-08-22 02:09:11 +0400

    comited final version

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,23 +4,24 @@
 <title> Welcome to the Andersen-test project </title>
 <link rel="stylesheet" href="/andersen.css">
 <body id="body">
-    <h1>  Welcome to the Andersen-test project  </h1>
-
+  <div class='main'>
+    <h1>Andersen-test project</h1>
 
 <form action="{{route('contact-submit')}}" method = "POST">
     @csrf
     <input type="text" name="name" id='name' class="form-input" placeholder="Enter your name" required></br>
     <input type="text" name="email" id='email' class="form-input" placeholder="Enter your email" required></br>
     <input type="text" name="message" id='message' class="form-input" placeholder="Enter your message" required></br>
-    <button>Submit</button>
+    <button class="btn-submit">Submit</button>
 </form>
+</div>
 @if($posts->count() > 0)   
 <div>
 <table>
   <tr>
     <th>Name</th>
     <th>Email</th> 
-    <th>Messages</th>
+    <th>Message</th>
   </tr>
   @foreach($posts as $post)
   <tr>
